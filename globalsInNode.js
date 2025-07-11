0476b40637ee8acb00f8779d34fcbf1f23360fd6
Ruchir Saxena <ruchirsaxena.coder@gmail.com>
2025-07-11 12:10:35 +0000

    Node Js Globals

---
+++ globalsInNode.js
@@ -0,0 +1,9 @@
+// Globals variables in Node Js
+
+console.log(__dirname);
+
+// Require  -> Uts a function which is used to import code from other files 
+console.log(require);
+
+// process -> It is an object it gives you information about current process
+console.log("PROCESS ->",process); 
